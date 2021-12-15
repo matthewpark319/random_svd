9f3037babfac2f574e02f0f610ca02f621bc574e
matthewpark319 <matthewpark319@gmail.com>
2021-12-15 13:57:00 -0500

    dont use numpy calculations bc they're already parallelized

--- random_svd.py
+++ random_svd.py
@@ -5,9 +5,9 @@ import pandas as pd
 from mpi4py import MPI
 
 # A = pd.read_csv('data/baseball_features.csv').to_numpy()
-A = np.random.normal(size=(10000, 10000))
+A = np.random.normal(size=(1000, 1000))
 
-k = 1000
+k = 10
 
 comm = MPI.COMM_WORLD
 num_procs = comm.Get_size()
@@ -22,7 +22,14 @@ row_end_index = min((rank + 1) * cols_per_rank, k)
 
 t0 = time.time()
 
-piece = A @ np.random.normal(size=(A.shape[1], row_end_index - row_start_index))
+G_cols = np.random.normal(size=(A.shape[1], row_end_index - row_start_index))
+piece = np.zeros((A.shape[0], G_cols.shape[1]))
+for j in range(G_cols.shape[1]):
+    col = G_cols[:,j]
+    for i in range(A.shape[0]):
+        row = A[i,:]
+        piece[i,j] = sum(row[l] * col[l] for l in range(A.shape[0]))
+
 print(time.time() - t0)
 pieces = comm.gather(piece)
 
