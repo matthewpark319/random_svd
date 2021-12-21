8e0763d5db8003c4376b922856843a5c0aea6d2d
matthewpark319 <matthewpark319@gmail.com>
2021-12-21 12:22:35 -0500

    parallel matmul

--- linear_algebra_funcs.py
+++ linear_algebra_funcs.py
@@ -1,8 +1,11 @@
 import math
+import time
 
+from mpi4py import MPI
 import numpy as np
 
 import util
+
 
 def qr_factorization(A):
     Q = np.empty(A.shape)
@@ -15,3 +18,72 @@ def qr_factorization(A):
         Q[:, i] = q_i / math.sqrt(sum(q_i_elmt**2 for q_i_elmt in q_i))
 
     return Q
+
+
+def parallel_matmul(A, B):
+    comm = MPI.COMM_WORLD
+    rank = comm.Get_rank()
+    num_procs = comm.Get_size()
+
+    B = comm.scatter([B for _ in range(num_procs)])
+
+    t0 = time.time()
+    if rank == 0:
+        for i in range(1, num_procs):
+            row_start_index, row_end_index = util.start_end_index(A.shape[0], num_procs, i)
+            print(f'Sending rows {row_start_index} - {row_end_index} to rank {i}')
+            comm.isend(row_end_index - row_start_index, dest=i, tag=i)
+            for row_index in range(row_start_index, row_end_index):
+                row_to_send = A[row_index,].copy()
+                comm.Isend([row_to_send, MPI.FLOAT], dest=i, tag=i)
+
+        row_start_index, row_end_index = util.start_end_index(A.shape[0], num_procs, 0)
+        A_piece = A[row_start_index:row_end_index,:]
+    else: # Calculate which piece of A to calculate a piece of AA with
+        rows = []
+        rows_to_recv = comm.recv(source=0, tag=rank)
+        A_piece = np.empty((rows_to_recv, B.shape[0]))
+        for i in range(rows_to_recv):
+            comm.Recv([A_piece[i,], MPI.FLOAT], source=0, tag=rank)
+
+    print(f'rank {rank} A_piece construction ({A_piece.shape}): {time.time() - t0}')
+
+    # Calculate a piece of QT_A
+    piece = np.empty((A_piece.shape[0], B.shape[1]))
+    print(A_piece.shape)
+    for i in range(A_piece.shape[0]):
+        for j in range(B.shape[1]):
+            print(f'rank {rank}: {A_piece[i,]} dot {B[:,j]}')
+            piece[i, j] = util.dot(A_piece[i,], B[:,j])
+            # piece[i, j] = A_piece[i,].dot(B[:,j])
+
+    print(f'rank {rank} piece: {piece}')
+
+    pieces = comm.gather(piece)
+    if rank == 0:
+        return np.concatenate(pieces)
+
+
+def power_iteration(A, epsilon=1e-10):
+    ''' The one-dimensional SVD '''
+
+    n, m = A.shape
+    x = util.normalize(np.random.normal(size=min(n, m)))
+    prev = None
+    v = x
+
+    if n > m:
+        B = np.dot(A.T, A)
+    else:
+        B = np.dot(A, A.T)
+
+    iterations = 0
+    while True:
+        iterations += 1
+        prev = v
+        v = np.dot(B, prev)
+        v = v / sum(elmt**2 for elmt in v)
+
+        if abs(np.dot(v, prev)) > 1 - epsilon:
+            print("converged in {} iterations!".format(iterations))
+            return v
--- util.py
+++ util.py
@@ -1,3 +1,6 @@
+import math
+
+
 def start_end_index(total_pts, num_procs, rank):
     pts_per_rank = int((total_pts + num_procs - 1) / num_procs)
     return rank * pts_per_rank, min((rank + 1) * pts_per_rank, total_pts)
@@ -5,3 +8,7 @@ def start_end_index(total_pts, num_procs, rank):
 def dot(v1, v2):
     assert len(v1) == len(v2)
     return sum(v1[i] * v2[i] for i in range(len(v1)))
+
+def normalize(v):
+    norm = sum(elmt**2 for elmt in v)
+    return v / math.sqrt(norm)
